01b7a034fcef300dedc4612d08e2b94c7b152c47
Jason Howard <jasonlhoward@gmail.com>
2020-02-02 10:45:50 -0700

    Initial commit.

--- CSharp-Basics/Program.cs
+++ CSharp-Basics/Program.cs
@@ -10,60 +10,61 @@ namespace CSharp_Basics
     {
         static void Main(string[] args)
         {
-            // Arithmetic
-            // postfix increment
-            int a = 1;
-            int b = a++;    // b is assigned to the value of a and then a is incremented
-            Console.WriteLine("a = {0}, b = {1}", a, b); // -> a = 2, b = 1
+            // Implicit Type Conversion
+            byte a = 1;
+            int b = a;  // because i is an int and bytes hold a subset of the integer range
+            Console.WriteLine(a);
+            Console.WriteLine(b);
 
-            // prefix increment
-            int c = 1;
-            int d = ++c;    // c is incremented and then d is assigned to the value of c
-            Console.WriteLine("c = {0}, d = {1}", c, d); // -> c = 2, d = 2
+            // What happens the other way around?
+            // This will result in a "Cannot implicitly convert 'int' to 'byte'" error
+            // The reason for this is that data loss is possible.
+            // An int is 4 bytes and a byte is, well, 1 byte.
+            // 1000 = 00000000 00000000 00000011 11101000
+            // To stuff 1000 into 1 byte, the first 2 bytes are simply removed, what's left is 232
+            // 232 = 00000000 00000000 00000000 11101000
+            int c = 1000;
+            //byte d = c; // I commented this line so this would compile
 
-            // postfix decrement
-            int e = 1;
-            int f = e--;    // f is assigned to the value of e and then e is decremented
-            Console.WriteLine("e = {0}, f = {1}", e, f); // -> e = 0, f = 1
+            // Explicit Conversion
 
-            // prefix decrement
-            int g = 1;
-            int h = --g;    // g is decremented and then h is assigned the value of g
-            Console.WriteLine("g = {0}, h = {1}", g, h); // -> g = 0, h = 0
+            // CASTING
+            // Using explicit conversion tells the complier that you're aware there could be data loss but you want to do it anyway.
+            // Explicit conversion can only be used on similar data types like int->byte or long->int
+            int e = 25;
+            byte f = (byte)e;
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            // Back to the earlier issue where you're converting from an int not in the byte range
+            int g = 1000;
+            byte h = (byte)g; // -> yields 232
+            Console.WriteLine(h);
 
-            // Comparison
-            // == is equal to
-            // != not equal
-            //  > greater than
-            // >= greater than or equal to
-            //  < less than
-            // <= less than or equal to
+            // CONVERTING
+            string number = "1234";
+            int i = Convert.ToInt32(number);
+            Console.WriteLine(i);
 
-            // Assignment
-            // a = 1    -> set a equal to 1
-            // a += 3   -> increment a by 3, then set this new value to a
-            // a -= 3   -> decrement a by 3, then set this new value to a
-            // a *= 3   -> multiply a by 3, then set this new value to a
-            // a /= 3   -> divide a by 3, then set this new value to a
-            // a = !b   -> set a = true is b is false, set a = false is b is true
+            //byte j = Convert.ToByte(number); // this will throw an exception at runtime so...
+            try
+            {
+                byte j = Convert.ToByte(number);
+            }
+            catch (Exception) //OverflowException
+            {
+                Console.WriteLine("The number could not be converted to a byte");
+            }
 
-            // Logical
-            // a && b   -> evaluate to true if a and b are true
-            // a || b   -> evaluate to true if a or b are true
-            // a ! b    -> evaluate to true is a is not equal to b
-
-            // Bitwise
-            // & -> bitwise "and"
-            // | -> bitwise "or"
-
-
-            // Examples
-            a = 1;
-            b = 2;
-            c = 3;
-
-            Console.WriteLine(a / b);
-            Console.WriteLine((float)a / (float)b);
+            try
+            {
+                string boolStr = "true";
+                bool isTrue = Convert.ToBoolean(boolStr);
+                Console.WriteLine(isTrue);
+            }
+            catch (Exception) //OverflowException
+            {
+                Console.WriteLine("The string 'true' cannot be converted to a Boolean");
+            }
         }
     }
 }
