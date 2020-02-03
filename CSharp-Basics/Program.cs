52d2b69c624c3bd83d07f42464b55dc23296659c
Jason Howard <jasonlhoward@gmail.com>
2020-02-02 17:00:55 -0700

    Initial commit.

--- CSharp-Basics/Program.cs
+++ CSharp-Basics/Program.cs
@@ -1,20 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharp_Basics
 {
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //Person john = new Person();
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            var method = ShippingMethod.Express;
+            Console.WriteLine(method.ToString()); // Express     
+
+            // Casting the shipping method key to the value
+            Console.WriteLine((int)method); // 3
+
+            // Casting the shipping method value to the key
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId); // Express
+
+            // Parse the method name (as a string) to the method value (i.e. Express)
+            var methodName = "Express";
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+            Console.WriteLine(shippingMethod); // Express
         }
     }
 }
