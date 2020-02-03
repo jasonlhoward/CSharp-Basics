f70518ecf7486dff64ba745348fa1be584fe2707
Jason Howard <jasonlhoward@gmail.com>
2020-02-02 17:09:43 -0700

    Added comments

--- CSharp-Basics/Program.cs
+++ CSharp-Basics/Program.cs
@@ -15,6 +15,27 @@ namespace CSharp_Basics
             john.FirstName = "John";
             john.LastName = "Smith";
             john.Introduce();
+
+            /*
+            Structures vs Classes
+            
+            Primitive data types are Structures and take no more than 8 bytes
+
+            Non-primitives are Classes
+
+            Structures -> Value Types
+            Classes -> Reference Types
+
+            Value Types
+                - Allocated on stack
+                - Memory allocation is done automatically
+                - immediately removed when out of scope
+
+            Reference Types
+                - YOU need to allocate memory (i.e. the "new" keyword)
+                - Memory allocated on heap
+                - Garbage collected by CLR
+            */
         }
     }
 }
