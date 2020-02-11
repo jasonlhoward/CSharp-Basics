4e1db08972a3953c3c060b6ab3d1760e887e4b9b
Jason Howard <jasonlhoward@gmail.com>
2020-02-11 05:37:44 -0700

    Exercise 5 completed.

--- CSharp-Basics/Exercises.cs
+++ CSharp-Basics/Exercises.cs
@@ -117,5 +117,22 @@ namespace CSharp_Basics
 
         //5- Write a program and ask the user to enter an English word. Count the number of vowels (a, e, o, u, i) in the word. 
         //So, if the user enters "inadequate", the program should display 6 on the console.
+        public static void Exercise5()
+        {
+            Console.Write("Enter an English word and I'll count the vowels: ");
+            var input = Console.ReadLine().ToLower();
+            if (String.IsNullOrWhiteSpace(input)) { return; }
+
+            var vowels = new[] { 'a', 'e', 'i', 'o', 'u' };
+            var vowelCnt = 0;
+
+            foreach (var character in input)
+            {
+                if (vowels.Contains(character))
+                    vowelCnt++;
+            }
+
+            Console.WriteLine(vowelCnt);
+        }
     }
 }
--- CSharp-Basics/Program.cs
+++ CSharp-Basics/Program.cs
@@ -10,7 +10,7 @@ namespace CSharp_Basics
     {
         static void Main(string[] args)
         {
-            Exercises.Exercise4();
+            Exercises.Exercise5();
         }
     }
 }
