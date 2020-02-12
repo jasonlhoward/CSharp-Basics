94e1198c22b15c22bc3eb2d3b77542511cb0a79f
Jason Howard <jasonlhoward@gmail.com>
2020-02-12 07:34:16 -0700

    Initial commit

--- CSharp-Basics/Exercises.cs
+++ CSharp-Basics/Exercises.cs
@@ -8,5 +8,8 @@ namespace CSharp_Basics
 {
     class Exercises
     {
+    //1- Write a program that reads a text file and displays the number of words.
+
+    //2- Write a program that reads a text file and displays the longest word in the file.
     }
 }
